2394854da10f50b3de3bd89a76aa6430b3f2d2bb
Timothy Johnson <timothy.johnson@broadcom.com>
2021-05-24 09:38:43 -0400

    Escape characters in sed expression
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- semantic-release.sh
+++ semantic-release.sh
@@ -5,13 +5,13 @@ if ! grep -q "['tag', tagName, ref]" node_modules/semantic-release/lib/git.js; t
   echo >&2 "semantic-release package is an unsupported version"
   exit 1
 fi
-sed -i "s/['tag', tagName, ref]/['tag', tagName, ref, '-s', '-m', process.env.GIT_TAG_MESSAGE]/" node_modules/semantic-release/lib/git.js
+sed -i 's/\['\''tag'\'', tagName, ref\]/['\''tag'\'', tagName, ref, '\''-s'\'', '\''-m'\'', process.env.GIT_TAG_MESSAGE]/' node_modules/semantic-release/lib/git.js
 
 # Sign off commits
 if ! grep -q "['commit', '-m', message]" node_modules/@semantic-release/git/lib/git.js; then
   echo >&2 "@semantic-release/git package is an unsupported version"
   exit 1
 fi
-sed -i "s/['commit', '-m', message]/['commit', '-s', '-m', message]/" node_modules/@semantic-release/git/lib/git.js
+sed -i 's/\['\''commit'\'', '\''-m'\'', message\]/['\''commit'\'', '\''-s'\'', '\''-m'\'', message]/' node_modules/@semantic-release/git/lib/git.js
 
 npx semantic-release "$@"
