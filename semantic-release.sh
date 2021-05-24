2297149b8ce37d31ac4210473dd9d31217682114
Timothy Johnson <timothy.johnson@broadcom.com>
2021-05-24 10:56:29 -0400

    Annotate Git tag, don't sign it
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- semantic-release.sh
+++ semantic-release.sh
@@ -12,7 +12,7 @@ if ! grep -q "['tag', tagName, ref]" node_modules/semantic-release/lib/git.js; t
   echo >&2 "semantic-release package is an unsupported version"
   exit 1
 fi
-sed -i 's/\['\''tag'\'', tagName, ref\]/['\''tag'\'', tagName, ref, '\''-s'\'', '\''-m'\'', process.env.GIT_TAG_MESSAGE]/' node_modules/semantic-release/lib/git.js
+sed -i 's/\['\''tag'\'', tagName, ref\]/['\''tag'\'', tagName, ref, '\''-a'\'', '\''-m'\'', process.env.GIT_TAG_MESSAGE]/' node_modules/semantic-release/lib/git.js
 
 # Sign off commits
 if ! grep -q "['commit', '-m', message]" node_modules/@semantic-release/git/lib/git.js; then
