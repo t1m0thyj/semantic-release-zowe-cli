4de273ead4ef74923ab28f49d119e223fa48a318
Timothy Johnson <timothy.johnson@broadcom.com>
2022-03-02 08:45:48 -0500

    Test faking an old version
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/verify-conditions.ts
+++ src/verify-conditions.ts
@@ -21,4 +21,14 @@ export default async (pluginConfig: any, context: Context): Promise<void> => {
                 break;
         }
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const getLastRelease = require("semantic-release/lib/get-last-release");
+    context.logger.log(JSON.stringify((context as any).branch.tags));
+    context.logger.log(JSON.stringify(getLastRelease(context)));
+    const tags = (context as any).branch.tags.filter((tag: any) => tag.version === pkgVersion);
+    if (!pkgVersion.includes("-") && tags.length > 0) {
+        tags[0].version = `${pkgVersion}-${(context as any).branch.name}`;
+    }
+    context.logger.log(JSON.stringify(getLastRelease(context)));
 }
